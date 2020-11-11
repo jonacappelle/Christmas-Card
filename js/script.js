191118224119d8d35bcf984824f41735a4d4a6ab
AlexanderMarinsek <alexander.marinsek@gmail.com>
2020-11-11 19:02:44 +0100

    Added almost-ideal spherical distribution of points and the corresponding scene drawing functionality; Included additional images and raw GIMP output for outlining Will Smith's face.

--- js/script.js
+++ js/script.js
@@ -22,28 +22,220 @@ window.addEventListener("resize", function () {
  */
 function createAndPopulateScene (engine, canvas) {
 
-    // Create the scene space
-    var scene = new BABYLON.Scene(engine);
-    scene.clearColor = new BABYLON.Color3(0.3, 0, 0);
+  // Create the scene space
+  var scene = new BABYLON.Scene(engine);
+  scene.clearColor = new BABYLON.Color3(0.3, 0, 0);
 
-    addCamera(scene, canvas)
-    addLights(scene)
-    addPhotoDome(scene)
-    addMusic(scene)
-    //addLegacyItems(scene)
-    addDramcoLogoPlanes(scene)
+  draw_bottom_layer(scene, 30)
+  draw_top_layer(scene, 20)
+  addCamera(scene, canvas)
+  addLights(scene)
+  addPhotoDome(scene)
+  addMusic(scene)
 
-    return scene;
+  return scene;
 };
 
 
-/* Add camera (view)
- *  p1: Scene instance
- *  p2: DOM canvas pointer
+/* Draw the bottom layer, containing various small logos.
+ * Make sure the plane size isn't too large to avoid overlapping with planes of the same and of other layers.
+ *
+ * @param {object} scene Babylon scene instance.
+ * @param {number} number_of_planes Number of planes to fetch and draw.
+ */
+function draw_bottom_layer( scene, number_of_planes ) {
+
+  let plane_size = 1.0
+  let randomize_plane_size = true;
+
+  let material = new BABYLON.StandardMaterial("", scene);
+  material.specularColor = new BABYLON.Color3(0, 0, 0); // Disable highlights in the material (flat image)
+  material.backFaceCulling = false; // Allways show the front and the back of an element
+  let texture = new BABYLON.Texture("Images/logo-white.png", scene);
+  texture.hasAlpha = true;
+  material.diffuseTexture = texture
+
+  draw_almost_equidistant_planes_on_sphere( scene, material, number_of_planes, plane_size, randomize_plane_size )
+
+}
+
+
+/* Draw the top layer, containing peoples faces.
+ * Make sure the plane size isn't too large to avoid overlapping with planes of the same and of other layers.
+ *
+ * @param {object} scene Babylon scene instance.
+ * @param {number} number_of_planes Number of planes to fetch and draw.
+ */
+function draw_top_layer( scene, number_of_planes ) {
+
+  let plane_size = 2.5
+  let randomize_plane_size = false;
+
+  let material = new BABYLON.StandardMaterial("", scene);
+  material.specularColor = new BABYLON.Color3(0, 0, 0); // Disable highlights in the material (flat image)
+  material.backFaceCulling = false; // Allways show the front and the back of an element
+  let texture = new BABYLON.Texture("Images/will_smith_outline.png", scene);
+  texture.hasAlpha = true;
+  material.diffuseTexture = texture
+
+  draw_almost_equidistant_planes_on_sphere( scene, material, number_of_planes, plane_size, randomize_plane_size )
+
+}
+
+
+/* Draw (almost) equidistant planes on a sphere and add them to the scene.
+ *
+ * @param {object} scene Babylon scene instance.
+ * @param {object} material Babylon material instance.
+ * @param {number} number_of_planes Number of planes to fetch and draw.
+ * @param {number} plane_size The average size of a plane (make sure input images have square dimensions!).
+ * @param {bool} randomize_plane_size Add random factor to plane sizes.
+ * @return {array} Babylon coordinate system X, Y, Z component arrays and phi and theta angle arrays.
+ */
+function draw_almost_equidistant_planes_on_sphere ( scene, material, number_of_planes, plane_size, randomize_plane_size ) {
+
+  // Fetch the plane center-point coordinates
+  let coordinates = calc_almost_optimal_spherical_point_distribution(number_of_planes)
+
+  let plane;
+  let current_plane_size = plane_size;
+
+  for (let i=0; i<number_of_planes; i++) {
+
+    // Add a random factor to plane sizes
+    if (randomize_plane_size) {
+      current_plane_size = plane_size - random_float_in_interval(plane_size * 0.2, plane_size * 0.8)
+    }
+
+    // Create new object and associate with scene
+    plane = BABYLON.MeshBuilder.CreatePlane( "plane", { size: current_plane_size }, scene );
+
+    plane.material = material // Associate plane with material (image)
+
+    // Position the current plane
+    plane.position.x = coordinates[0][i];
+    plane.position.y = coordinates[1][i];
+    plane.position.z = coordinates[2][i];
+
+    // Convert the point's latitude (0~PI) to difference from perpendicular viewing angle around x axis (-PI/2~PI/2)
+    plane.rotation.x = coordinates[3][i] - Math.PI/2 // Phi value minus PI/2
+
+    // Convert the point's longitude (0~2*PI) to difference from perpendicular viewing angle around y axis (-PI/2~PI/2)
+    plane.rotation.y = (coordinates[4][i] + Math.PI/2) % (2*Math.PI) // Theta value plus PI/2 and wrap around 2*PI
+    // plane.rotation.y = Math.PI / 2 - Math.atan(plane.position.z / plane.position.x)
+    // if ( plane.position.x < 0 ) { plane.rotation.y += Math.PI }
+
+  }
+
+}
+
+
+/* Get random float in the desired interval.
+ *
+ * @param {number} max Top boundary.
+ * @param {number} min Bottom boundary.
+ * @return {number} Random float.
+ */
+function random_float_in_interval(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+
+/* Calculate an approximative optimal distribution (equal distances) of points on top of a sphere.
+ * The phi and theta angles represent the latitude (0~π), coming down from the pole, and longitude (0~2π).
+ * Inspired by: https://stackoverflow.com/a/44164075/5153899
+ *
+ * @param {number} num_of_points Number of points.
+ * @return {array} Babylon coordinate system X, Y, Z component arrays and phi and theta angle arrays.
+ */
+function calc_almost_optimal_spherical_point_distribution(num_of_points) {
+
+  // Change the distance of the points to the origin (default: 1)
+  let multipllication_factor = 10;
+
+  // Arrange equidistant input points
+  let indices = range(0.5, num_of_points, 1);
+
+  // Pre-allocate output (at this point, XYZ are the usual Cartesian coordinates with Z representing height)
+  let x = [];
+  let y = [];
+  let z = [];
+  let phi = [];
+  let theta = [];
+
+  let i;
+
+  // Calc latitude
+  for (i=0; i<num_of_points; i++) {
+    phi.push( Math.acos(1 - 2*indices[i]/num_of_points) );
+  }
+
+  // Calc longitude
+  for (i=0; i<num_of_points; i++) {
+    theta.push( Math.PI * (1 + 5**0.5) * indices[i] );
+  }
+
+  // Calc X
+  let tmp_x;
+  for (i=0; i<num_of_points; i++) {
+    tmp_x = Math.cos(theta[i]) * Math.sin(phi[i]) * multipllication_factor;
+    if ( tmp_x == 0 ) { tmp_x = 0.01 }; // When X is 0, the graphic is not visible
+    x.push( tmp_x );
+  }
+
+  // Calc Y
+  for (i=0; i<num_of_points; i++) {
+    // y.push( Math.sin(theta[i]) * Math.sin(phi[i]) * multipllication_factor );
+    y.push( - Math.sin(theta[i]) * Math.sin(phi[i]) * multipllication_factor );
+  }
+
+  // Calc Z
+  for (i=0; i<num_of_points; i++) {
+    z.push( Math.cos(phi[i]) * multipllication_factor );
+  }
+
+  // Substitute Y (height) and Z to get Babylon components
+  return [x, z, y, phi, theta]
+}
+
+
+/* Python range equivalent function.
+ *
+ * @param {number} start Start of interval (included, 0 by default).
+ * @param {number} stop Interval stop (excluded).
+ * @param {number} step Step size (default: 1).
+ * @return {array} Sequence of numbers.
+ */
+function range(start, stop, step) {
+  if (typeof stop == 'undefined') {
+    // one param defined
+    stop = start;
+    start = 0;
+  }
+
+  if (typeof step == 'undefined') {
+    step = 1;
+  }
+
+  if ((step > 0 && start >= stop) || (step < 0 && start <= stop)) {
+    return [];
+  }
+
+  var result = [];
+  for (var i = start; step > 0 ? i < stop : i > stop; i += step) {
+    result.push(i);
+  }
+
+  return result;
+};
+
+
+/* Add camera (view).
+ *
+ * @param {object} scene Babylon scene instance.
+ * @param {object} material DOM canvas pointer.
  */
 function addCamera (scene, canvas) {
-  //var camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0,0,40), scene);
-
   // Two-image display, needs cardboard viewer (VR). On PC change the view using the mouse.
   //var camera = new BABYLON.VRDeviceOrientationFreeCamera("DevOr_camera", new BABYLON.Vector3(0, 0, 0), scene);
 
@@ -51,23 +243,16 @@ function addCamera (scene, canvas) {
   let camera = new BABYLON.DeviceOrientationCamera("DevOr_camera", new BABYLON.Vector3(0, 0, 0), scene);
 
   // Defines the target the camera should look at.
-  camera.setTarget(new BABYLON.Vector3(1, 0, 0));
-
-  // WITH VR GLASSES
-  // var camera = new BABYLON.VRDeviceOrientationArcRotateCamera ("Camera", Math.PI/2, Math.PI/4, 25, new BABYLON.Vector3 (0, 0, 0), scene);
-
-  // Parameters : name, position, scene
-  // var camera = new BABYLON.UniversalCamera("UniversalCamera", new BABYLON.Vector3(0, 0, -80), scene);
-  // Targets the camera to a particular position. In this case the scene origin
-  // camera.setTarget(BABYLON.Vector3.Zero());
+  camera.setTarget(new BABYLON.Vector3(0.1, 0, 0));
 
   // Attach the camera to the canvas
   camera.attachControl(canvas, true);
 }
 
 
-/* Add spherical background projection
- *  p1: Scene instance
+/* Add spherical background projection.
+ *
+ * @param {object} scene Babylon scene instance.
  */
 function addPhotoDome (scene) {
   //new BABYLON.PhotoDome("testdome", "./Images/BackGround.jpg", { resolution: 256, size: 1000 }, scene)
@@ -76,197 +261,27 @@ function addPhotoDome (scene) {
 }
 
 
-/* Add lights (otherwise the objects won't be visible)
- *  p1: Scene instance
+/* Add lights (otherwise the objects won't be visible).
+ *
+ * @param {object} scene Babylon scene instance.
  */
 function addLights (scene) {
-  new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
-  new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
+  // new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 0, 0), scene);
+  // new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
+  // new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 0, 0), scene);
+  var light = new BABYLON.PointLight("pointLight", new BABYLON.Vector3(0,0,0), scene);
+  var light = new BABYLON.PointLight("Omni", new BABYLON.Vector3(0,0,0), scene);
+  light.intensity = 1
 }
 
 
-/* Add fancy pants music
- *  p1: Scene instance
+/* Add fancy pants music.
+ *
+ * @param {object} scene Babylon scene instance.
  */
 function addMusic (scene) {
     new BABYLON.Sound("Music", "./Songs/song.wav", scene, null, {
       loop: true,
       autoplay: true
     });
-}
-
-
-/* Populate the landscape with DRAMCO logos.
- *  p1: Scene instance
- */
-function addDramcoLogoPlanes (scene) {
-
-  let material = new BABYLON.StandardMaterial("", scene);
-  let texture = new BABYLON.Texture("Images/logo-white.png", scene);
-  texture.hasAlpha = true;
-  material.diffuseTexture  = texture
-
-  let eigen = 10
-
-  var box1 = BABYLON.MeshBuilder.CreatePlane("plane", {size: 2}, scene);
-  box1.position.x = -10;
-  box1.position.y = -0;
-  box1.rotation.y = Math.atan(box1.position.x / box1.position.z)
-  box1.material = material
-
-  var box2 = BABYLON.MeshBuilder.CreatePlane("plane", {size: 3}, scene);
-  box2.position.x = -10 / Math.sqrt(2);
-  box2.position.y = 10 / Math.sqrt(2);
-  box2.rotation.y = Math.atan(box2.position.x / box2.position.z)
-  box2.rotation.x = Math.atan(box2.position.x / box2.position.y)
-  box2.material = material
-
-  var box3 = BABYLON.MeshBuilder.CreatePlane("plane", {size: 2.5}, scene);
-  box3.position.x = -10 / Math.sqrt(3);
-  box3.position.y = 10 / Math.sqrt(3);
-  box3.position.z = 10 / Math.sqrt(3);
-  box3.rotation.x = Math.atan(box3.position.x / box3.position.y)
-  box3.rotation.y = Math.atan(box3.position.x / box3.position.z)
-  box3.rotation.z = -Math.atan(box3.position.y / box3.position.z)
-  box3.rotation.z = 0
-  box3.material = material
-
-  var box4 = BABYLON.MeshBuilder.CreatePlane("plane", {size: 2}, scene);
-  box4.position.x = 10;
-  box4.position.y = -0;
-  box4.rotation.y = Math.atan(box4.position.x / box4.position.z)
-  box4.material = material
-
-}
-
-
-/* Populate the landscape with legacy items.
- *  p1: Scene instance
- */
-function addLegacyItems (scene) {
-
-  // Add lights to the scene
-  var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
-  var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
-
-  // Add the spheres
-  var redMat = new BABYLON.StandardMaterial("redMat", scene);
-  redMat.emissiveColor = new BABYLON.Color3(1, 0, 0);
-
-  var greenMat = new BABYLON.StandardMaterial("greenMat", scene);
-  greenMat.emissiveColor = new BABYLON.Color3(0, 1, 0);
-
-  var blueMat = new BABYLON.StandardMaterial("blueMat", scene);
-  blueMat.emissiveColor = new BABYLON.Color3(0, 0, 1);
-
-  var blue2Mat = new BABYLON.StandardMaterial("whiteMat", scene);
-  blue2Mat.emissiveColor = new BABYLON.Color3(0, 0.4, 0.5);
-
-  var redSphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2}, scene);
-  redSphere.material = redMat;
-
-  var greenSphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2}, scene);
-  greenSphere.material = greenMat;
-
-  var blueSphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2}, scene);
-  blueSphere.material = blueMat;
-
-  var blue2MatSphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2}, scene);
-  blue2MatSphere.material = blue2Mat;
-
-  // Move the spheres
-  redSphere.position.x = -3;
-  redSphere.position.y = 18;
-  greenSphere.position.x = -5;
-  greenSphere.position.y = 12;
-  blueSphere.position.x = 10;
-  blueSphere.position.y = 18;
-  blue2MatSphere.position.x = 5;
-  blue2MatSphere.position.y = 19;
-
-  // ./Images/Dramco_logo as location DOESN'T work for some reason
-
-  var pic_KULEUVEN_logo = new BABYLON.StandardMaterial("photo", scene);
-
-  pic_KULEUVEN_logo.diffuseTexture = new BABYLON.Texture("./Images/KULEUVEN_logo.png", scene);
-
-  var options_KULEUVEN_logo = {
-      width: 15,
-      height: 6,
-      depth: 1,
-  };
-
-  var box_kuleuven = BABYLON.MeshBuilder.CreateBox('box1', options_KULEUVEN_logo, scene);
-  box_kuleuven.material = pic_KULEUVEN_logo;
-  box_kuleuven.position.x = 5
-  box_kuleuven.rotation.y = Math.PI
-
-  var pic_DRAMCO_logo = new BABYLON.StandardMaterial("photo", scene);
-
-  pic_DRAMCO_logo.diffuseTexture = new BABYLON.Texture("./Images/Dramco_logo.jpg", scene);
-
-  var options_DRAMCO_logo = {
-      width: 9,
-      height: 9,
-      depth: 1,
-  };
-
-  var box_DRAMCO = BABYLON.MeshBuilder.CreateBox('box2', options_DRAMCO_logo, scene);
-  box_DRAMCO.material = pic_DRAMCO_logo;
-  box_DRAMCO.position.x = 5
-  box_DRAMCO.position.y = 10
-  box_DRAMCO.rotation.y = Math.PI
-
-  var pic_WC_logo = new BABYLON.StandardMaterial("photo", scene);
-
-  pic_WC_logo.diffuseTexture = new BABYLON.Texture("./Images/WC_logo.png", scene);
-
-  var options_WC_logo = {
-      width: 15,
-      height: 6,
-      depth: 1,
-  };
-
-  var box_WC = BABYLON.MeshBuilder.CreateBox('box3', options_WC_logo, scene);
-  box_WC.material = pic_WC_logo;
-  box_WC.position.x = 5;
-  box_WC.position.y = -10;
-  box_WC.rotation.y = Math.PI;
-
-
-  //Creation of a repeated textured material
-  var kerstboom = new BABYLON.StandardMaterial("kerstboom", scene);
-  kerstboom.diffuseTexture = new BABYLON.Texture("./Images/tree.png", scene);
-  //kertboom.specularColor = new BABYLON.Color3(0, 0, 0);
-  kerstboom.backFaceCulling = false; //Allways show the front and the back of an element
-  kerstboom.diffuseTexture.hasAlpha = true;
-  kerstboom.backFaceCulling = false;
-
-
-  //Creation of a plane
-  var tree_plane = BABYLON.MeshBuilder.CreatePlane("plane", {width: 75, height: 124}, scene);
-  tree_plane.material = kerstboom;
-  tree_plane.position.x = 50;
-  tree_plane.rotation.y = -Math.PI/10
-
-
-  // GUI for merry christmass button
-  var plane_button = BABYLON.MeshBuilder.CreatePlane("plane2", {width: 18, height: 7}, scene);
-  plane_button.position.y = -0;
-  plane_button.position.x = -30;
-  plane_button.rotation.y = Math.PI + Math.PI/10;
-
-  var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateForMesh(plane_button);
-
-  var button1 = BABYLON.GUI.Button.CreateSimpleButton("but1", "Press me !");
-  button1.width = 18;
-  button1.height = 7;
-  button1.color = "white";
-  button1.fontSize = 200;
-  button1.background = "darkblue";
-  button1.onPointerUpObservable.add(function() {
-      alert("Merry Christmas!");
-  });
-  advancedTexture.addControl(button1);
-
 }
